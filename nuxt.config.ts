376f8ef14863f9f123d8aee48a0a47bd9ae98241
Gaurav.Bhandari <gaurav2503bhandari@gmail.com>
2024-01-16 12:29:06 +0530

    added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
+import GoogleAnalytics from '@nuxtjs/google-analytics';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  buildModules: [
+    '@nuxt/typescript-build',
+    GoogleAnalytics,
+  ],
+  googleAnalytics: {
+    id:  process.env.GOOGLE_ANALYTICS_ID,
+  },
 })
